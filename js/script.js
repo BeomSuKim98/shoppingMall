8a2afaa5bcf0951f92cb6d021f536bb9cb326985
BeomSuKim98 <qja0427@gmail.com>
2025-02-21 16:41:12 +0900

    아이템 섹션 디자인 완료/스와이퍼 작업 필

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function SwiperController() {
+function SwiperTopBanner() {
   const swiper1 = new Swiper(".swiper-container-firstTopNotice", {
     direction: "vertical",
     loop: true,
@@ -8,18 +8,28 @@ function SwiperController() {
       disableOnInteraction: false,
     },
   });
+}
 
+SwiperTopBanner();
+
+function SwiperMainBanner() {
   const swiper2 = new Swiper(".swiper-container-mainBanner", {
     direction: "horizontal",
     loop: true,
     slidesPerView: 3,
-    spacebetween: 10,
+    centeredSlides: true,
+    spaceBetween: 10,
     slidespergroup: 1,
     autoplay: {
-      delay: 10000,
+      delay: 5000,
       disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".mainBanner_page",
+      type: "bullets",
+      clickable: true, // 페이지네이션을 클릭하여 슬라이드를 이동할 수 있게 함
     },
   });
 }
 
-SwiperController();
+SwiperMainBanner();
