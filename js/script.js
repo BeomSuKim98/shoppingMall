cfac0cc83fda95c535e09b382b5224936976cd1c
BeomSuKim98 <qja0427@gmail.com>
2025-02-24 14:55:55 +0900

    아이템 섹션 스와이퍼추가/확대률 100%기준으로 디자인 재작업/html구조 망가져 있던 것 수정

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function SwiperTopBanner() {
 SwiperTopBanner();
 
 function SwiperMainBanner() {
-  const swiper2 = new Swiper(".swiper-container-mainBanner", {
+  const swiper1 = new Swiper(".swiper-container-mainBanner", {
     direction: "horizontal",
     loop: true,
     slidesPerView: 3,
@@ -33,3 +33,38 @@ function SwiperMainBanner() {
 }
 
 SwiperMainBanner();
+
+function SwiperCategory(n) {
+  const swiper1 = new Swiper(`.swiper-container-categoryBox-${n}`, {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 1,
+    // centeredSlides: true,
+    // spaceBetween: 10,
+    // slidespergroup: 1,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: `.swiper-categoryBox-btn-group-${n} .swiper-btn-next`,
+      prevEl: `.swiper-categoryBox-btn-group-${n} .swiper-btn-prev`,
+    },
+    pagination: {
+      el: `.categoryBox_page-${n}`,
+      type: "bullets",
+      clickable: true, // 페이지네이션을 클릭하여 슬라이드를 이동할 수 있게 함
+    },
+  });
+}
+
+SwiperCategory(1);
+
+// 이미지 로딩 스크린
+$("body").imagesLoaded(function () {
+  console.log("모든 이미지 로딩 완료");
+
+  setTimeout(function () {
+    $(".loading-screen").remove();
+  }, 500);
+});
