1fa3db2400e12d4d3d18949d9183439704f15b29
BeomSuKim98 <qja0427@gmail.com>
2025-03-05 18:55:09 +0900

    메인페이지 섹션작업/서브페이지 모바일작업

--- js/productPage.js
+++ js/productPage.js
@@ -1,18 +1,16 @@
-
 function Tabmenu() {
-	$(".tabMenu_top .tabMenu_item").click(function () {
-		let $this = $(this);
-		let $thisIndex = $this.index();
-		const $tabMenuBox = $this.closest(".tabmenu");
-		const $tabMenuContent = $tabMenuBox.find(".tabMenu_bottom");
+  $(".tabMenu_top .tabMenu_item").click(function () {
+    let $this = $(this);
+    let $thisIndex = $this.index();
+    const $tabMenuBox = $this.closest(".tabMenu");
+    const $tabMenuContent = $tabMenuBox.find(".tabMenu_bottom");
 
-		$this.siblings(".active").removeClass("active");
-		$this.addClass("active");
-		console.log("클릭됌");
+    $this.siblings(".active").removeClass("active");
+    $this.addClass("active");
 
-		$tabMenuContent.find(".tabMenu_item").removeClass("active");
-		$tabMenuContent.find(".tabMenu_item").eq($thisIndex).addClass("active");
-	});
+    $tabMenuContent.find(".tabMenu_item").removeClass("active");
+    $tabMenuContent.find(".tabMenu_item").eq($thisIndex).addClass("active");
+  });
 }
 
 Tabmenu();
