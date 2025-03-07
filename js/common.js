a8ab9cc815d6e894b7365ff6230a9dc5104566ee
BeomSuKim98 <qja0427@gmail.com>
2025-03-07 18:49:06 +0900

    상품 페이지 디자인 중

--- js/common.js
+++ js/common.js
@@ -1,31 +1,3 @@
-// // 공통 header와 footer를 비동기 방식으로 적용
-// function loadCommonHeaderAndFooter() {
-//   fetch("./header.html")
-//     .then((resp) => resp.text())
-//     .then((data) => {
-//       document.getElementById("header").innerHTML = data;
-
-//       const swiper1 = new Swiper(".swiper-container-firstTopNotice", {
-//         direction: "vertical",
-//         loop: true,
-//         slidesPerView: 1,
-//         autoplay: {
-//           delay: 3000,
-//           disableOnInteraction: false,
-//         },
-//       });
-//     });
-
-//   fetch("./footer.html")
-//     .then((resp) => resp.text())
-//     .then((data) => {
-//       document.getElementById("footer").innerHTML = data;
-//       // console.log(data);
-//     });
-// }
-
-// document.addEventListener("DOMContentLoaded", loadCommonHeaderAndFooter);
-
 function fetchWithFallback(url, fallbackUrl, callback) {
   fetch(url)
     .then((resp) => {
@@ -53,6 +25,12 @@ function loadCommonHeaderAndFooter() {
         delay: 3000,
         disableOnInteraction: false,
       },
+    });
+
+    $(".logoImg").attr("src", "./img/logo.png");
+
+    $(".logoImg").on("error", function () {
+      $(".logoImg").attr("src", "../img/logo.png");
     });
   });
 
--- js/productPage.js
+++ js/productPage.js
@@ -14,3 +14,102 @@ function Tabmenu() {
 }
 
 Tabmenu();
+
+function thBox() {
+  const swiper1 = new Swiper(".thBox_menu", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+  });
+}
+
+thBox();
+
+function thnailImg() {
+  let thIndex = $(".imgInsult");
+
+  for (let i = 0; i < thIndex.length; i++) {
+    let $img = $(thIndex[i]);
+
+    $img.on("error", function () {
+      $img.closest(".swiper-slide").remove();
+    });
+
+    $img.attr("src", "../img/detailPage/page1/item" + (i + 1) + ".jpg");
+  }
+  console.clear();
+}
+
+thnailImg();
+
+function eventImg() {
+  let thIndex = $(".eventImg");
+
+  for (let i = 0; i < thIndex.length; i++) {
+    let $img = $(thIndex[i]);
+
+    $img.on("error", function () {
+      $img.closest(".content_imgBox").remove();
+    });
+
+    $img.attr("src", "../img/detailPage/page1/eventImg" + (i + 1) + ".jpg");
+  }
+  console.clear();
+}
+
+eventImg();
+
+function detailImg() {
+  let thIndex = $(".detailImg");
+
+  for (let i = 0; i < thIndex.length; i++) {
+    let $img = $(thIndex[i]);
+
+    $img.on("error", function () {
+      $img.closest(".content_imgBox").remove();
+    });
+
+    $img.attr("src", "../img/detailPage/page1/detail" + (i + 1) + ".png");
+  }
+  console.clear();
+}
+
+detailImg();
+
+let $deliveryCost = 0;
+
+function deliveryCost() {
+  $(".deliverySelector").change(function () {
+    if (!$(this).closest(".product_infoBox").is(":hidden")) {
+      $deliveryCost = parseFloat($(this).val());
+      console.log("deliveryCost : ", $deliveryCost);
+      totalPrice();
+    }
+  });
+}
+
+deliveryCost();
+
+function totalPrice() {
+  $(".totalPrice").each(function () {
+    let $baseCost;
+    let $saleValue;
+    let $totalCost;
+    let $cost = 0;
+    if (!$(this).closest(".product_infoBox").is(":hidden")) {
+      $baseCost = parseFloat(
+        $(this)
+          .closest(".product_infoBox_totalPrice")
+          .siblings(".product_infoBox_priceBox")
+          .text()
+          .trim()
+          .replace(/[^0-9.-]+/g, "")
+      );
+      console.log("baseCost : ", $baseCost);
+    }
+    $totalCost = $baseCost + $deliveryCost;
+    $(this).text($totalCost + "원");
+    console.log("totalCost : ", $totalCost);
+  });
+}
+
+totalPrice();
